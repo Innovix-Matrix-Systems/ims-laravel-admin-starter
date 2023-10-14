a5c460e316cf7ac662b25a7efc2586a83170ee28
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2023-10-14 13:38:35 +0600

    local development performance improvemen

--- config/debugbar.php
+++ config/debugbar.php
@@ -160,7 +160,7 @@ return [
         'exceptions'      => true,  // Exception displayer
         'log'             => true,  // Logs from Monolog (merged in messages if enabled)
         'db'              => true,  // Show database (PDO) queries and bindings
-        'views'           => true,  // Views with their data
+        'views'           => false,  // Views with their data
         'route'           => true,  // Current route information
         'auth'            => false, // Display Laravel authentication status
         'gate'            => true,  // Display Laravel Gate checks
@@ -173,7 +173,7 @@ return [
         'logs'            => false, // Add the latest log messages
         'files'           => false, // Show the included files
         'config'          => false, // Display config settings
-        'cache'           => false, // Display cache events
+        'cache'           => true, // Display cache events
         'models'          => true,  // Display models
         'livewire'        => true,  // Display Livewire (when available)
     ],
