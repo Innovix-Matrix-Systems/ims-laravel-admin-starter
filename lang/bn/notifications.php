9f1e7648345c1cbca8246b8d8356b3977c59dbc5
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2023-10-14 19:24:17 +0600

    tabs & database notification added

---
+++ lang/bn/notifications.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'save.success' => 'সফলভাবে সংরক্ষিত হয়েছে',
+    'profile.save.failed'    => 'প্রোফাইল তথ্য আপডেট করতে ব্যর্থ হয়েছে',
+    'profile.password.save.success' => 'পাসওয়ার্ড সফলভাবে আপডেট করা হয়েছে',
+    'user.created.success'  => 'ব্যবহারকারী সফলভাবে তৈরি করা হয়েছে',
+
+];
